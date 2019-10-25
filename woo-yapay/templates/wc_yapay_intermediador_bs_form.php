a8341c60b66056060fe8c69d9e61201cfaa355ec
wagnerjaccoud <wagner.jaccoud@gmail.com>
2019-10-25 11:35:30 -0300

    Update wc_yapay_intermediador_bs_form.php

--- woo-yapay/templates/wc_yapay_intermediador_bs_form.php
+++ woo-yapay/templates/wc_yapay_intermediador_bs_form.php
@@ -8,13 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <fieldset id="wc-yapay_intermediador-bs-payment-form">
 
     <input type="hidden" id="tcbPaymentMethod" name="wc-yapay_intermediador-bs-payment-method" class="required-entry" value="" autocomplete="off">
-    <label for="tcbPaymentMethod">Selecione a forma de pagamento <span class="required">*</span></label>
-    <ul>
-        
-        <li class="tcbPaymentMethod">
-            <img src="<?php echo $url_images; ?>boleto-flag.svg" class="tcPaymentFlag tcPaymentFlagSelected" id="tcbPaymentMethod">
-        </li>
-    </ul>
+    <<?php _e( 'Clique em finalizar compra para gerar seu boleto', 'woocommerce-wc-yapay_intermediador-bs' ); ?>
 
 
     <div id="cpf_yapayB" class="cpf_yapay" style="display: none">  	
