80e9e29b8407f0cfe827c0393a5d103c0ce7d268
wagnerjaccoud <wagner.jaccoud@gmail.com>
2020-04-09 00:07:42 -0300

    Add files via upload

--- woo-yapay/templates/wc_yapay_intermediador_bs_form.php
+++ woo-yapay/templates/wc_yapay_intermediador_bs_form.php
@@ -8,7 +8,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <fieldset id="wc-yapay_intermediador-bs-payment-form">
 
     <input type="hidden" id="tcbPaymentMethod" name="wc-yapay_intermediador-bs-payment-method" class="required-entry" value="" autocomplete="off">
-    <<?php _e( 'Clique em finalizar compra para gerar seu boleto', 'woocommerce-wc-yapay_intermediador-bs' ); ?>
+    <?php _e( 'Clique em finalizar compra para gerar seu boleto', 'woocommerce-wc-yapay_intermediador-bs' ); ?>
 
 
     <div id="cpf_yapayB" class="cpf_yapay" style="display: none">  	
